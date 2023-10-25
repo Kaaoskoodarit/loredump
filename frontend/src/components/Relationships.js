7bc983c07bb83cbfd92b6530f9a5a9b7de497549
VeliNalle <kjk861@gmail.com>
2023-10-25 13:39:10 +0300

    Made Relationships-component

--- frontend/src/components/Relationships.js
+++ frontend/src/components/Relationships.js
@@ -1,6 +1,3 @@
-import {useState} from 'react';
-
-
 const Relationships = (props) => {
 
     // poistettavaa testi koodia
@@ -14,64 +11,32 @@ const Relationships = (props) => {
 
     // poistettavaa koodia loppuu
 
-
-    const [state,setState] = useState({
-        relationships:props.relationships,
-        reltype:"",
-        target: ""
-    })
-    
-    const onChange = (event) => {
-        setState((state) => {
-            return {
-                ...state,
-                relationships :[{
-                    [event.target.name]:event.target.value
-                }]
-            }
-        });
-        // setState((state)=>{
-        //     return{
-        //     ...state,
-        //     relationship_list: [...props.relationships, {}]
-        // }
-        // })
-    }
-
-    // const onDropdownChange = (event) => {
-    //     setState((state) => {
-    //         return {
-    //             ...state,
-    //             relationships:[{target: event.target.value}]
-    //         }
-    //     })
-    // }
-
-    
-	
 	return(
-        <table>
-      <tbody>
-		<tr>
-			<td><input type="text"
-						id="reltype"
-						name="reltype"
-						className="form-control"
-						onChange={onChange}
-						value={state.reltype}/></td>
-            <td>
-			<select name="target"
-                        id="target"
-                        className="form-select"
-                        aria-label="relationship target"
-                        onChange={onChange}>
-                        <option value="">Select relationship link</option>
-                    {rel_dropdown}
-                </select>
-            </td>
-		</tr>
-        </tbody>
-        </table>
+        <div>
+            <label htmlFor="relationships" className="form-label">Relationships</label>
+            <table>
+                <tbody>
+		            <tr>
+			            <td><input type="text"
+						    id="reltype"
+						    name="reltype"
+						    className="form-control"
+						    onChange={props.onChange}
+						    value={props.relationships.reltype}/></td>
+                        <td>
+			                <select name="target"
+                                id="target"
+                                className="form-select"
+                                aria-label="relationship target"
+                                onChange={props.onChange}>
+                            <option value="">Select relationship link</option>
+                            {rel_dropdown}
+                            </select>
+                        </td>
+		            </tr>
+                </tbody>
+            </table>
+        </div>
 	)
 }
 
